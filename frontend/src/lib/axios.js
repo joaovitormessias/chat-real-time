b9fad865cf30172a2039b1c3d3b3791f91ba73a8
joaovitormessias <joaovitormessiasdamasio.com>
2025-05-31 13:34:09 -0300

    Ultimos ajustes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 // Permite comunicação com o backend, enviarmos cookies e requests
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api": "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
